681853d7ab7e902d25c34c33dabaa6e2a6f4a467
Mohammad Saif <saifsami321md@gmail.com>
2022-11-19 21:07:12 +0530

    counter is done with useReducer

--- dark_light_mode_toggling/src/App.js
+++ dark_light_mode_toggling/src/App.js
@@ -1,10 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import {useReducer} from "react"
+
+const initialState = 0;
+
+const reducer = (state,action) =>{
+
+  console.log(state,action)
+  switch(action.type){
+    case "Increament":
+      return state+1;
+    case "Decreament":
+      return state-1;
+    default:
+      return state;
+  }
+}
 
 function App() {
+
+const [state,dispatch] = useReducer(reducer,initialState)
+
   return (
     <div className="App">
       <h1>Setup ready</h1>
+      <button onClick={()=>dispatch({type:"Increament"})}>+</button>
+      {state}
+      <button onClick={()=>dispatch({type:"Decreament"})}>-</button>
     </div>
   );
 }
