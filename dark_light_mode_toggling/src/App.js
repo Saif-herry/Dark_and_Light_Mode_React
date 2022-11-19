ffd372c655606b7fd9face98bf1fff0f605973b4
Mohammad Saif <saifsami321md@gmail.com>
2022-11-19 20:52:14 +0530

    basic setup is ready

---
+++ dark_light_mode_toggling/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Setup ready</h1>
+    </div>
+  );
+}
+
+export default App;
